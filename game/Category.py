8592796fb4f1fbe7919fcadb72f918cc367ce6ed
ionutsavin <ionutsavin6@gmail.com>
2024-10-24 21:44:45 +0300

    final updates

--- game/Category.py
+++ game/Category.py
@@ -5,7 +5,7 @@ class Category:
     name: str
     description: str
 
-    def get_score(self,dice:list[int]) -> int:
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
         pass
 
     def __hash__(self):
@@ -15,18 +15,148 @@ class Category:
         return self.name == other.name
 
 
-class Sum(Category):
+class DiceValueCategory(Category):
+    value: int
+
+    def __init__(self, name: str, description: str, value: int):
+        self.name = name
+        self.description = description
+        self.value = value
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        if joker_rule:
+            return 5 * self.value
+        return self.value * dice.count(self.value)
+
+
+class Chance(Category):
 
     def __init__(self):
-        self.name = 'Sum'
+        self.name = 'Chance'
         self.description = 'Sum of all dice'
-    def get_score(self, dice: list[int]) -> int:
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
         return sum(dice)
 
-class Ones(Category):
+
+class Ones(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Ones', 'Sum of all ones', 1)
+
+
+class Twos(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Twos', 'Sum of all twos', 2)
+
+
+class Threes(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Threes', 'Sum of all threes', 3)
+
+
+class Fours(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Fours', 'Sum of all fours', 4)
+
+
+class Fives(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Fives', 'Sum of all fives', 5)
+
+
+class Sixes(DiceValueCategory):
+    def __init__(self):
+        super().__init__('Sixes', 'Sum of all sixes', 6)
+
+
+class ThreeOfAKind(Category):
 
     def __init__(self):
-        self.name = 'Ones'
-        self.description = 'Sum of all ones'
-    def get_score(self, dice: list[int]) -> int:
-        return dice.count(1)
+        self.name = 'Three of a Kind'
+        self.description = 'Sum of all dice if 3 are the same'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        for i in range(1, 7):
+            if dice.count(i) >= 3:
+                return sum(dice)
+        return 0
+
+
+class FourOfAKind(Category):
+
+    def __init__(self):
+        self.name = 'Four of a Kind'
+        self.description = 'Sum of all dice if 4 are the same'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        for i in range(1, 7):
+            if dice.count(i) >= 4:
+                return sum(dice)
+        return 0
+
+
+class FullHouse(Category):
+
+    def __init__(self):
+        self.name = 'Full House'
+        self.description = '25 if 3 of a kind and 2 of a kind'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        if joker_rule:
+            return 25
+        threeDices = False
+        twoDices = False
+        for i in range(1, 7):
+            if dice.count(i) == 3:
+                threeDices = True
+            if dice.count(i) == 2:
+                twoDices = True
+        if threeDices and twoDices:
+            return 25
+        return 0
+
+
+class SmallStraight(Category):
+
+    def __init__(self):
+        self.name = 'Small Straight'
+        self.description = '30 if 4 in a row'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        if joker_rule:
+            return 30
+        sortedDice = sorted(set(dice))
+        SmallStraights = [{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}]
+        for i in SmallStraights:
+            if i.issubset(sortedDice):
+                return 30
+        return 0
+
+
+class LargeStraight(Category):
+
+    def __init__(self):
+        self.name = 'Large Straight'
+        self.description = '40 if 5 in a row'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        if joker_rule:
+            return 40
+        sortedDice = sorted(set(dice))
+        LargeStraights = [{1, 2, 3, 4, 5}, {2, 3, 4, 5, 6}]
+        for i in LargeStraights:
+            if i.issubset(sortedDice):
+                return 40
+        return 0
+
+
+class Yahtzee(Category):
+
+    def __init__(self):
+        self.name = 'Yahtzee'
+        self.description = '50 if all dice are the same'
+
+    def get_score(self, dice: list[int], joker_rule = False) -> int:
+        if len(set(dice)) == 1:
+            return 50
+        return 0
--- player/HumanPlayer.py
+++ player/HumanPlayer.py
@@ -11,7 +11,11 @@ class HumanPlayer(Player):
         self._display_player_turn()
         self.roll_dice(list(range(5)))
         self.handle_rerolls()
-        self.chose_category()
+        is_yahtzee = self.verify_yahtzee()
+        joker_rule = False
+        if is_yahtzee:
+            joker_rule = self.handle_yahtzee()
+        self.chose_category(joker_rule)
         self.reset_dice()
 
     def handle_rerolls(self):
@@ -32,13 +36,13 @@ class HumanPlayer(Player):
             print(f'Rerolling dice: {dice_to_reroll}')
             self.roll_dice(dice_to_reroll)
 
-    def chose_category(self):
+    def chose_category(self, joker_rule=False):
         print(f'Available categories: ')
         valid_categories = []
         for idx, category in enumerate(self.scorecard.keys()):
             if self.scorecard.get(category) is not None:
                 continue
-            print(f'{idx + 1}. {category.name} - Points: {category.get_score(self.dice)}')
+            print(f'{idx + 1}. {category.name} - Points: {category.get_score(self.dice, joker_rule)}')
             valid_categories.append(category.name)
         category_name = None
         while category_name not in valid_categories:
@@ -46,5 +50,5 @@ class HumanPlayer(Player):
                 print(f'Invalid category: {category_name}')
             category_name = input('Choose a category: ').lstrip().rstrip()
         category = next(category for category in self.scorecard.keys() if category.name == category_name)
-        self.check_scorecard(category)
+        self.check_scorecard(category, joker_rule)
         self._display_choice(category)
