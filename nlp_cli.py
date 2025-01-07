243a5633ab5c83f203ae4bd0da128fa5873ccced
Alexandru-Vasilica <alexandru.vasilica97@gmail.com>
2025-01-07 21:32:24 +0200

    Improved synonym corellation and added sentence generation

--- nlp_cli.py
+++ nlp_cli.py
@@ -1,6 +1,7 @@
 import argparse
 
-from nlp.utils import detect_language, SUPPORTED_LANGUAGES, get_stylometric_features, rephrase_text, extract_keywords
+from nlp.utils import detect_language, SUPPORTED_LANGUAGES, get_stylometric_features, rephrase_text, extract_keywords, \
+    unify_keywords, generate_pos_tags, generate_sentence_with_keyword
 from nlp.wordnet import EnWordNet, RoWordNet
 
 
@@ -8,7 +9,7 @@ def main():
     parser = argparse.ArgumentParser(description='NLP Command Line Interface')
     parser.add_argument('input', type=str, help='Input to be processed')
     parser.add_argument('--file', action='store_true', help='Treats input as file path')
-
+    parser.add_argument('--verbose', action='store_true', help='Prints additional information')
     args = parser.parse_args()
     if args.file:
         with open(args.input, 'r') as file:
@@ -26,18 +27,29 @@ def main():
     print(f'Word length: {stylometric_features["word_length"]}')
     print(f'Character length: {stylometric_features["char_length"]}')
     print('--Word Frequency--')
-    for word, freq in sorted(stylometric_features['word_freq'].items(), key=lambda x: x[1], reverse=True):
+    shown_word_frequencies = len(stylometric_features['word_freq'].items()) if args.verbose else 10
+    for word, freq in sorted(stylometric_features['word_freq'].items(), key=lambda x: x[1], reverse=True)[:shown_word_frequencies]:
         print(f'Word: {word}, Frequency: {freq}')
     print('--Character Frequency--')
-    for char, freq in sorted(stylometric_features['char_freq'].items(), key=lambda x: x[1], reverse=True):
+    shown_char_frequencies = len(stylometric_features['char_freq'].items()) if args.verbose else 10
+    for char, freq in sorted(stylometric_features['char_freq'].items(), key=lambda x: x[1], reverse=True)[:shown_char_frequencies]:
         print(f'Character: {char}, Frequency: {freq}')
     print("--Rephrased Text--")
     rephrased_text = rephrase_text(text, wn, syn_ration=0.2, hyper_ration=0, ant_ration=0.1)
     print(rephrased_text)
     print("----Keywords----")
-    keywords = extract_keywords(text)
-    for keyword in keywords:
-        print(keyword)
+    keyword_len = 10 if args.verbose else 5
+    keywords = extract_keywords(text, language=wn.language)
+    modified_keywords, modified_text = unify_keywords(keywords, text)
+    pos_tags = generate_pos_tags(modified_text)
+    for modified_keyword in modified_keywords:
+        sentence = generate_sentence_with_keyword(modified_keyword, pos_tags)
+        if sentence is not None:
+            print(f'Keyword: {modified_keyword}')
+            print(sentence)
+            keyword_len -= 1
+            if keyword_len == 0:
+                break
 
 
 if __name__ == '__main__':
